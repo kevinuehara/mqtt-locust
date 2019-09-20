2726706880e7966c532c6cb661e27e60b5b4e3de
kevin <kevin@cpqd.com.br>
2019-09-20 13:16:46 -0300

    Improves mqtt-client code

--- iot_device.py
+++ iot_device.py
@@ -1,90 +1,24 @@
-import paho.mqtt.client as mqtt
 import os
-import logging
-import time
-import json
 import random
-import threading
 
-from utils import Utils
 from locust import TaskSet, task, seq_task
-
-REQUEST_TYPE = "mqtt"
-TENANT = "admin"
-  
-class MQTT_Client:
-
-    def __init__(self, device_id):
-        self.mqttc = mqtt.Client()
-        self.mqttc.connect("test.mosquitto.org", 1883, 60)
-        self.mqttc.on_connect = self.on_connect
-        self.mqttc.on_message = self.on_message
-
-    def get_client(self):
-        return self.mqttc
-
-    def on_connect(self, client, userdata, flags, rc):
-        print("Connected with result code "+str(rc))
-        
-    def on_message(self, client, userdata, msg):
-        print(msg.topic+" "+str(msg.payload))
-
+from mqtt_client import MQTT_Client
 
 class IoT_Device(TaskSet):
 
     def on_start(self):
         self.device_id = random.randint(1,101)
-        self.client_mqtt = MQTT_Client(self.device_id)       
+        self.client_mqtt = MQTT_Client()  
 
+        self.client_mqtt.connect()     
 
     @seq_task(1)
     @task(2)
-    def start_loop(self):
-        logging.info("Starting loop...")
-        self.client_mqtt.get_client().loop()
-        
+    def loop(self):
+        self.client_mqtt.start_loop()
 
     @seq_task(2)
     @task(1)
     def publish(self):
-        topic = "/{0}/{1}/attrs".format(TENANT, 1)
-        payload = "{'temperature': random.randint(1, 21)}"
-        start_time = time.time()
-
-        logging.info(f"Publishing in topic {topic}")
-        try:
-            res = self.client_mqtt.get_client().publish(
-                    topic=topic,
-                    payload=payload,
-                    qos=0,
-                    retain=False
-                )
-
-            [ err, mid ] = res
-
-            if err:
-                logging.error(str(err))
-                Utils.fire_locust_failure(
-                    request_type=REQUEST_TYPE,
-                    name='messages',
-                    response_time=Utils.time_delta(start_time, time.time()),
-                    exception=ValueError(err)
-                )
-
-                logging.info("publish ERROR: err,mid:"+str(err)+","+str(mid)+"")
-                logging.error(err)
-
-            else:
-                logging.info("#### SUCESSO")
-
-        except Exception as e:
-            logging.error(str(e))
-            Utils.fire_locust_failure(
-                request_type=REQUEST_TYPE,
-                name='messages',
-                response_time=Utils.time_delta(start_time, time.time()),
-                exception=e,
-            )
-        
-
+       self.client_mqtt.publishing()
     
--- mqtt_client.py
+++ mqtt_client.py
@@ -1,17 +1,125 @@
+"""
+    Handles Paho MQTT-Client operations like publish/subscription, connection,
+    loop function.
+"""
 import paho.mqtt.client as mqtt
+import os
+import logging
+import time
+import json
+import random
+import threading
 
+from utils import Utils
+from locust import TaskSet, task, seq_task
 
-class MQTTClient(mqtt.Client):
+TENANT = "admin"
+REQUEST_TYPE = 'MQTT'
+MESSAGE_TYPE_PUB = 'PUB'
+PUBLISH_TIMEOUT = 5000
 
-    def __init__(self, *args, **kwargs):
-        super(MQTTClient, self).__init__(*args, **kwargs)
-        self.on_connect = self.locust_on_connect
-        self.on_publish = self.locust_on_publish
-        self.pubmmap = {}
-        self.is_connected = False
-
-    def locust_on_connect():
-        pass
+class MQTT_Client:
     
-    def locust_on_publish():
-        pass
+    def __init__(self):
+        self.mqttc = mqtt.Client()
+        self.mqttc.on_connect = self.on_connect
+        self.mqttc.on_publish = self.locust_on_publish
+        self.pubmmap = {}
+
+    def get_client(self):
+        return self.mqttc
+
+    def connect(self):
+        self.mqttc.connect("localhost", 1883, 60)
+
+    def on_connect(self, client, userdata, flags, rc):
+        print("Connected with result code "+str(rc))
+    
+    def start_loop(self):
+        logging.info("Starting loop...")
+        self.mqttc.loop(0.5)
+
+    def publishing(self):
+        topic = "/{0}/{1}/attrs".format(TENANT, '4929cf')
+        payload = {'humidity': int(5.5)}
+
+        start_time = time.time()
+
+        logging.info(f"Publishing in topic {topic}")
+        try:
+            res = self.mqttc.publish(
+                    topic=topic,
+                    payload=json.dumps(payload),
+                    qos=1,
+                    retain=False
+                )
+
+            [ err, mid ] = res
+
+            if err:
+                logging.error(str(err))
+                Utils.fire_locust_failure(
+                    request_type=REQUEST_TYPE,
+                    name='publish',
+                    response_time=Utils.time_delta(start_time, time.time()),
+                    exception=ValueError(err)
+                )
+
+                logging.info("publish ERROR: err,mid:"+str(err)+","+str(mid)+"")
+                logging.error(err)
+
+            self.pubmmap[mid] = {
+                'name': MESSAGE_TYPE_PUB, 
+                'qos': 1, 
+                'topic': topic,
+                'payload': payload,
+                'start_time': start_time, 
+                'timed_out':10000, 
+                'messages': 'messages'
+            }
+
+        except Exception as e:
+            logging.error(str(e))
+            Utils.fire_locust_failure(
+                request_type=REQUEST_TYPE,
+                name='publish',
+                response_time=Utils.time_delta(start_time, time.time()),
+                exception=e,
+            )
+
+    def locust_on_publish(self, client, userdata, mid):
+        logging.info("--locust_on_publish--")
+
+        end_time = time.time()
+        print(self.pubmmap)
+        message = self.pubmmap.pop(mid, None)
+
+        if message is None:
+            logging.info("message is none")
+            Utils.fire_locust_failure(
+                request_type=REQUEST_TYPE,
+                name="message_found",
+                response_time=0,
+                exception=ValueError("Published message could not be found"),
+            )
+            return
+
+        total_time = Utils.time_delta(message.start_time, end_time)
+        logging.info("total time: " + str(total_time))
+
+        if message.timed_out(total_time):
+            logging.error("publish timed out")
+            Utils.fire_locust_failure(
+                request_type=REQUEST_TYPE,
+                name=message.name,
+                response_time=total_time,
+                exception=TimeoutError("publish timed out"),
+            )
+        else:
+            logging.info("message sent")
+            Utils.fire_locust_success(
+                request_type=REQUEST_TYPE,
+                name=message.name,
+                response_time=total_time,
+                response_length=len(message.payload),
+            )
